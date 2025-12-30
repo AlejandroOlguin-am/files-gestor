0308ee14550c829d96fb765c3912e783b95fb677
AlejandroOlguin-am <alejandro_olguin17@hotmail.com>
2025-12-30 17:27:13 -0400

    feat: se agrego el metodo frutas

---
+++ scripts/app.py
@@ -0,0 +1,21 @@
+import os
+from typing import Union
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+@app.get("/familia")
+def get_familia():
+    rows = ["Alejandro", "Felicia", "Emiliana"]
+    return rows
+
+@app.get("/genios")
+def get_genios():
+    rows = ["Newton", "Einstein", "Tesla", "Curie", "Edison", "Leonardo da Vinci", "Galileo", "Hawking", "Turing"]
+    return rows
+
+@app.get("/frutas")
+def get_frutas():
+    rows = ["Manzana", "Banana", "Cereza", "Dátil", "Elderberry"]
+    return rows
